d0b1793266bee8a8bf7107125868eda2b34f9483
berickdev <brunoerick161@hotmail.com>
2025-11-03 19:26:46 -0300

    modelo cliente completo

--- app/models.py
+++ app/models.py
@@ -5,3 +5,7 @@ class Cliente(db.Model):
     __tablename__ = 'cliente'
     id = db.Column(db.Integer, primary_key = True)
     nome = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    telefone = db.Column(db.String(20), nullable=False)
+
+    
